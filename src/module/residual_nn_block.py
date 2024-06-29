536a2485eb9db07c361d6c6bed2035a2c672c14e
Omer Sela <omer.sela13@gmail.com>
2024-06-29 12:30:45 +0300

    added events files to gitignore

--- src/module/residual_nn_block.py
+++ src/module/residual_nn_block.py
@@ -11,21 +11,21 @@ class ResidualNNBlock(nn.Module):
     def __init__(self, in_features, out_features):
         super().__init__()
         self.lin1 = nn.Linear(in_features, out_features)
-        self.bn1 = nn.BatchNorm1d(out_features)
+        # self.bn1 = nn.BatchNorm1d(out_features)
         self.relu = nn.ReLU()
         self.lin2 = nn.Linear(out_features, out_features)
-        self.bn2 = nn.BatchNorm1d(out_features)
+        # self.bn2 = nn.BatchNorm1d(out_features)
         self.downsample = nn.Linear(in_features, out_features)
 
     def forward(self, x):
         identity = x
 
         x = self.lin1(x)
-        x = self.bn1(x)
+        # x = self.bn1(x)
         x = self.relu(x)
 
         x = self.lin2(x)
-        x = self.bn2(x)
+        # x = self.bn2(x)
 
         x += self.downsample(identity)
 
