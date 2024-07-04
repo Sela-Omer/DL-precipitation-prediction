9c34a8c4d13a593251ffec4f5f576b9c541448dc
Omer Sela <omer.sela13@gmail.com>
2024-07-05 00:42:15 +0300

    added simple_nn skip connection

---
+++ src/module/simple_nn_skip_connection.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+import lightning as pl
+import torch
+from lightning.pytorch.utilities.types import STEP_OUTPUT, OptimizerLRScheduler
+from torch import nn
+
+from src.module.simple_nn import SimpleNN
+from src.module.skip_connection_module import SkipConnectionModule
+from src.service.service import Service
+
+
+class SimpleNN_SkipConnection(SimpleNN):
+    """
+    A simple neural network model with a skip connection.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        body = list(self.layers[:-1])
+        body_out_features = self.layers[-1].in_features
+        skip_len = self.lookback_range * len(self.target_parameters)
+        last_lin = nn.Linear(body_out_features + skip_len,
+                             len(self.target_parameters))
+        nn.init.constant_(last_lin.weight, 0)
+
+        # Set the last k weights to 1/k
+        with torch.no_grad():
+            last_lin.weight[:, -skip_len:] = 1 / skip_len
+        # Initialize all biases to 0
+        nn.init.constant_(last_lin.bias, 0)
+
+        self.layers = SkipConnectionModule(nn.Sequential(*body), nn.Sequential(last_lin), self._select_skip_connection,
+                                           1)
+
+    def _select_skip_connection(self, x):
+        return torch.index_select(x, 1, torch.tensor(self.target_parameter_indices).to(x.device))
---
+++ src/module/skip_connection_module.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+
+
+class SkipConnectionModule(nn.Module):
+    """
+    A module that implements a skip connection between two modules.
+    :arg module_1: The first module.
+    :arg module_2: The second module.
+    :arg select_skip_connection_function: A function that selects the skip connection.
+    :arg concat_dim: The dimension to concatenate the skip connection on.
+
+    """
+
+    def __init__(self, module_1: nn.Module, module_2: nn.Module, select_skip_connection_function, concat_dim: int):
+        super().__init__()
+        self.module_1 = module_1
+        self.module_2 = module_2
+        self.select_skip_connection_function = select_skip_connection_function
+        self.concat_dim = concat_dim
+
+    def forward(self, x):
+        skip = self.select_skip_connection_function(x)
+        x = self.module_1(x)
+        x = torch.cat([x, skip], dim=self.concat_dim)
+        x = self.module_2(x)
+        return x
