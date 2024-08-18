c0b90417c1118b669da10a7e6d05096dafc62c1c
Omer Sela <omer.sela13@gmail.com>
2024-08-18 22:32:17 +0300

    synced with remote

---
+++ src/transform/sum_tfm.py
@@ -0,0 +1,21 @@
+from typing import Callable
+
+import numpy as np
+import torch
+
+
+class SumTfm(Callable):
+    def __init__(self, service, sum_parameter):
+        self.tfm_name = f'sum_{sum_parameter}'
+        self.service = service
+        self.sum_parameter = f'{sum_parameter}#sum'
+        self.data_parameters = service.data_parameters.copy()
+
+    def __call__(self, x):
+        # PARAMS x TIMES x HEIGHT x WIDTH
+        assert len(x.shape)==4, f"shape of x must be (PARAMS x TIMES x HEIGHT x WIDTH) instead got {x.shape}"
+        param_i = self.service.get_parameter_index(self.sum_parameter)
+
+        x[param_i] = x[param_i].sum(axis=(-2,-1))[...,None,None].repeat((1,1,x.shape[-2],x.shape[-1]))
+
+        return x
