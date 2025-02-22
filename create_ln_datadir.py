9a8b2e2126102c317c459cc1e08294c03f17d09a
Omer Sela <omer.sela13@gmail.com>
2025-02-22 19:14:53 +0200

    fixed slurm scripts

--- create_ln_datadir.py
+++ create_ln_datadir.py
@@ -1,32 +1,32 @@
 import os
 
+OVERRIDE_PARAM_NAMES = {'day': 'date', 'latitude': 'lat', 'longitude': 'lon', 'z_geo': 'z'}
+
 def symlink_all(dir, params, new_dir, suffix=''):
     for param in params:
-        os.symlink(os.path.join(dir, param), os.path.join(new_dir, param) + suffix)
+        out_param = param
+        if param in OVERRIDE_PARAM_NAMES:
+            out_param = OVERRIDE_PARAM_NAMES[param]
+        os.symlink(os.path.join(dir, param), os.path.join(new_dir, out_param) + suffix)
 
-def symlink_all_with_priority(dir1, dir2, params1, params2, new_dir):
-    all_params = params1.union(params2)
 
-    for param in all_params:
-        if param in params2:
-            # create symlink to high-res
-            os.symlink(os.path.join(dir2, param), os.path.join(new_dir, param) + '_0.25')
-        else:
-            # create symlink to low res
-            os.symlink(os.path.join(dir1, param), os.path.join(new_dir, param))
+dir1 = '/home/mansour/ML3300-24a/omersela3/v5/v5_0'
+dir2 = '/home/mansour/ML3300-24a/omersela3/v5/v5_2'
+dir3 = '/home/mansour/ML3300-24a/omersela3/v5/v5_4'
+dir4 = '/home/mansour/ML3300-24a/omersela3/v5/v5_6'
+dir5 = '/home/mansour/ML3300-24a/omersela3/v5/v5_8'
 
-dir1 = '/home/mansour/ML3300-24a/omersela3/OrTensors'
-dir2 = '/home/mansour/ML3300-24a/omersela3/OrTensors/6h_before_1.25'
-
-new_dir = '/home/mansour/ML3300-24a/omersela3/tensors-v3/w_6h_before_1.25'
+new_dir = '/home/mansour/ML3300-24a/omersela3/tensors-v5'
 os.makedirs(new_dir, exist_ok=True)
 
 params1 = set(os.listdir(dir1))
 params2 = set(os.listdir(dir2))
+params3 = set(os.listdir(dir3))
+params4 = set(os.listdir(dir4))
+params5 = set(os.listdir(dir5))
 
 symlink_all(dir1, params1, new_dir)
 symlink_all(dir2, params2, new_dir, '-6h')
-
-
-
-
+symlink_all(dir3, params2, new_dir, '-12h')
+symlink_all(dir4, params2, new_dir, '-18h')
+symlink_all(dir5, params2, new_dir, '-24h')
---
+++ msl-18-18.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Constants
+NUM_GPUS=2            # Number of GPUs to use
+BATCH_SIZE=25         # Batch size for training
+APP_NUM_NODES=1       # Number of application nodes
+DATA_PATH="/home/mansour/ML3300-24a/omersela3/tensors-v5"  # Data path
+CPU_WORKERS=12        # Number of CPU workers
+ACCELERATOR="gpu"     # Type of accelerator
+STRATEGY="ddp"        # Strategy to use
+OVERWRITE_CONFIG_PATH="src/config/cnn_skip_connection/config-v5-intensity-1979-2024-lookback3h-forecast1h-circular-norm-cnn-skip-empty_norm-max_pool-depth_8.ini"
+MODE="DATA_ANALYSIS,FIT,EVAL,EVAL_STORM_CLASSIFICATION"
+
+# Print environment details for debugging
+echo "Starting job on $(hostname) at $(date)"
+echo "Running in directory $(pwd)"
+echo "Using the following Python executable: $(which python)"
+echo "Using the following conda environment: $(conda info --envs | grep '*' | awk '{print $1}')"
+echo "SLURM_JOB_ID: $SLURM_JOB_ID"
+echo "SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST"
+echo "SLURM_NTASKS: $SLURM_NTASKS"
+echo "SLURM_CPUS_ON_NODE: $SLURM_CPUS_ON_NODE"
+
+# Activate the conda environment
+bash
+eval "$(conda shell.bash hook)"
+conda activate base
+export MPLCONFIGDIR=/home/mansour/ML3300-24a/omersela3/matplotlib_cache
+
+# Print the arguments to be passed to the Python script
+echo "Running script with the following arguments:"
+echo "  --APP_overwrite_config_path=$OVERWRITE_CONFIG_PATH"
+echo "  --APP_mode=$MODE"
+echo "  --APP_num_nodes=$APP_NUM_NODES"
+echo "  --DATA_path=$DATA_PATH"
+echo "  --APP_cpu_workers=$CPU_WORKERS"
+echo "  --APP_accelerator=$ACCELERATOR"
+echo "  --APP_strategy=$STRATEGY"
+echo "  --APP_devices=$NUM_GPUS"
+echo "  --APP_batch_size=$BATCH_SIZE"
+
+# Run the Python script with the defined constants
+python __init__.py \
+  --APP_overwrite_config_path=$OVERWRITE_CONFIG_PATH \
+  --APP_mode=$MODE \
+  --APP_num_nodes=$APP_NUM_NODES \
+  --DATA_path=$DATA_PATH \
+  --APP_cpu_workers=$CPU_WORKERS \
+  --APP_accelerator=$ACCELERATOR \
+  --APP_strategy=$STRATEGY \
+  --APP_devices=$NUM_GPUS \
+  --APP_batch_size=$BATCH_SIZE
